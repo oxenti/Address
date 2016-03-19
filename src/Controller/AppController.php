44c398a65b467e2a4d8c7212da625d68fac16ac3
Adewale Andrade <adewale.andrade@gmail.com>
2016-03-19 16:01:47 -0300

    Allow gets

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,6 +3,7 @@
 namespace Address\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Event\Event;
 
 class AppController extends BaseController
 {
@@ -14,5 +15,14 @@ class AppController extends BaseController
     public function isAuthorized($user)
     {
         return true;
+    }
+
+    /**
+     * beforeFilter Method
+     */
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow(['*']);
     }
 }
